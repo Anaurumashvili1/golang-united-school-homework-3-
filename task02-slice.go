acbe20a6da23621bbac23ef87039d69b0b1d7537
Anaurumashvili1 <anaurumashvili@gmailcom>
2022-07-08 12:53:11 +0400

    fixed bs

--- task02-slice.go
+++ task02-slice.go
@@ -4,7 +4,7 @@ func reverse(input []int64) (result []int64) {
 	//Place your code here
 	reversed := make([]int64, len(input))
 	for i := len(input) - 1; i >= 0; i-- {
-		for j := i; i < len(input); i++ {
+		for j := 0; j < len(input); j++ {
 			reversed[j] = input[i]
 		}
 	}
